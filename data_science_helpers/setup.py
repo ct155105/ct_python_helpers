9e361f8cf36d5510b082e659933c956d7c9b03d8
Christopher Teuschler <cteuschler@corbus.com>
2021-07-08 01:19:00 -0400

    added eggs for each of the packages

--- data_science_helpers/setup.py
+++ data_science_helpers/setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='data_science_helpers', version='1.0', packages=find_packages())
+setup(name='ds_helpers', version='1.0', packages=find_packages())
