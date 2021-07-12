9531818889860e469836d3020e5c26879184a716
Christopher Teuschler <cteuschler@corbus.com>
2021-07-12 00:52:30 -0400

    Add convert date string column to epoch

--- dataframe_wrappers/__tests__/df_transform_test.py
+++ dataframe_wrappers/__tests__/df_transform_test.py
@@ -9,3 +9,11 @@ def test_trim_column_names():
     test = df.columns == ['col1','col2','COL3']
 
     assert test.all()
+
+
+def test_date_column_str_to_epoch():
+    d = {"date_col": ['1995/08/25 05:00:00+00']}
+    df = pd.DataFrame(d)
+    df = ct.date_column_str_to_epoch(df,'date_col')
+
+    assert df.iloc[0]['date_col'] == 809326800000000000
--- dataframe_wrappers/pkg/df_helpers/df_transformers.py
+++ dataframe_wrappers/pkg/df_helpers/df_transformers.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 def trim_col_names(df: pd.DataFrame) -> pd.DataFrame:
     '''Removes spaces from column names
@@ -18,3 +20,19 @@ def trim_col_names(df: pd.DataFrame) -> pd.DataFrame:
 
     return renamed_df
         
+
+def date_column_str_to_epoch(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    '''Converts a column that is currently a date as string to epoch int
+    
+    Args:
+        df: The DataFrame housing the column to convert
+        column: The name of the column to convert
+
+    Returns:
+        The dataframe with the column converted to epoch
+    '''
+
+    df2 = df.copy()
+    df2[column] = pd.to_datetime(df2[column]).values.astype(np.int64)
+
+    return df2
